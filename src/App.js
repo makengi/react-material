2324d9dcb504ab00534fc267e2bec4fe6fe97e13
makengi <37012606+makengi@users.noreply.github.com>
2020-08-23 18:04:30 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+          <AppBar color="primary" position="static">
+          </AppBar>
+      </div>
+    );
+  }
+}
+
+export default App;
